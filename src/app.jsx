444b65af2fe936556f00214ec9c2ab5736dcb78e
cjbriseno <fr3shtricks@gmail.com>
2022-12-16 10:11:16 -0800

    from exercise4 added tailwind

---
+++ src/app.jsx
@@ -0,0 +1,25 @@
+import PocketBase from 'https://unpkg.com/pocketbase@0.8.3/dist/pocketbase.es.mjs'
+
+const USERNAME = 'cbriseno@csu.fullerton.edu'
+const PASSWORD = 'Briseno777'
+
+const pb = new PocketBase('http://127.0.0.1:8090')
+
+const authData = await pb.admins.authWithPassword(USERNAME, PASSWORD)
+console.log(authData)
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
+
+function App () {
+  return (
+    <div className='App'>
+      <h1>Hello, {authData.admin.email}</h1>
+    </div>
+  )
+}
